c237e1f7c66303a1927fe373e5d56d0a48ae3b2a
hyx546 <love.1179612384@qq.com>
2019-08-23 16:12:06 +0800

    curry

---
+++ js/curry/index.js
@@ -0,0 +1,15 @@
+// 普世
+function checkByRegExp(reg,string){
+    return reg.test(string);
+}
+console.log(checkByRegExp(/^1[3-9]\d{9}$/,'18623452345'));
+// .com.cn
+//  * 0到多次     + 1到多次   
+console.log(checkByRegExp(/^(\w)+(\.\w+)*@(\w)+((\.\w+)+)$/,'1179612384@qq.com'));
+
+checkByRegExp(/^1[3-9]\d{9}$/,'18623452345');
+checkByRegExp(/^1[3-9]\d{9}$/,'13312341234');
+checkByRegExp(/^1[3-9]\d{9}$/,'13643451212');
+checkByRegExp(/^(\w)+(\.\w+)*@(\w)+((\.\w+)+)$/,'test@qq.com');
+checkByRegExp(/^(\w)+(\.\w+)*@(\w)+((\.\w+)+)$/,'test@163.com');
+checkByRegExp(/^(\w)+(\.\w+)*@(\w)+((\.\w+)+)$/,'test@163.com');

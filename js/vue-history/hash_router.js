7a0024d2136acd9ce9ccfb067f8837a9668065aa
hyx546 <love.1179612384@qq.com>
2019-09-06 21:19:15 +0800

    vue-history

---
+++ js/vue-history/hash_router.js
@@ -0,0 +1,42 @@
+// 使用hash
+class HashRouter{
+    constructor() {
+        // key(/page1) => val (func 由外界传进来)
+        this.routers = {}
+        window.addEventListener('hashchange',this.load.bind(this))
+        // bind绑定
+    }
+    register (hash,callback = function() {}) {
+        this.routers[hash] = callback;
+    }
+
+    registerNotFound(callback = function() {}) {
+        this.routers['404'] = callback;
+    }
+    registerError(callback = function() {}) {
+        this.routers['error'] = callback;
+    }
+    load() {
+        // console.log(this);
+        
+        let hash =location.hash.slice(1),handler;//hander处理函数
+        
+        // container 显示相应的page内容
+        // 由外界决定 
+        
+        // handler();
+        // hasOwnProperty仅限于对象自身的属性，而不是去查找原型链上的属性
+        if(!this.routers.hasOwnProperty(hash)){
+            handler = this.routers['404'];
+        }else{
+            handler = this.routers[hash];
+        }
+        try{
+            handler.apply(this);
+        }
+        catch(e){
+            console.error(e);
+            (this.routers['error'] || function() {}).call(this,e);
+        }
+    }
+}

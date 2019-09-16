486897afce1743bf83f1d4bb144fd591bf30833b
hyx546 <love.1179612384@qq.com>
2019-09-16 23:21:25 +0800

    vue-toast-component

---
+++ zm/vue/vue-toast-component/src/components/HelloWorld.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <!-- v-show v-bind v-if 指令-->
+    <button @click="handleToast">购买</button>
+    <!-- <toast msg="测试弹窗1" :visiable="visiable"></toast> -->
+  </div>
+</template>
+
+<script>
+//alter
+
+// import Toast from './Toast/index'
+import showMessage from './Toast/index.js'
+export default {
+  name: 'HelloWorld',
+  components: {
+    // Toast,
+  },
+  data () {
+    return {
+      visiable: false
+    }
+  },
+  methods: {
+    handleToast() {
+      // this.visiable = true
+      // setTimeout(() => {
+      //   this.visiable = false
+      // }, 3000);
+      showMessage('没到开售时间')
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>

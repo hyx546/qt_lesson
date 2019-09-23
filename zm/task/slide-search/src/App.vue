cb79fcafabaa9243d9673a34ece902ca2723f19d
hyx546 <love.1179612384@qq.com>
2019-09-23 17:09:02 +0800

    task

---
+++ zm/task/slide-search/src/App.vue
@@ -0,0 +1,69 @@
+<template>
+  <div>
+    <slide-search :showSearchPage="showSearchPage"/>
+    <ul class="main">
+      <li>123456</li>
+      <li>123456</li>
+      <li>123456</li>
+      <li>123456</li>
+      <li>123456</li>
+      <li>123456</li>
+      <li>123456</li>
+      <li>123456</li>
+      <li>123456</li>
+      <li>123456</li>
+      <li>123456</li>
+      <li>123456</li>
+      <li>123456</li>
+      <li>123456</li>
+      <li>123456</li>
+      <li>123456</li>
+      <li>123456</li>
+      <li>123456</li>
+      <li>123456</li>
+      <li>123456</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import SlideSearch from './components/SlideSearch/index'
+import HelloWorld from './components/HelloWorld'
+
+export default {
+  data() {
+    return {
+      showSearchPage: false
+    }
+  },
+  components: {
+    HelloWorld,
+    SlideSearch
+  },
+  // 已经转换为真正 dom
+  mounted: function() {
+    //ref
+    const ul = document.querySelector('.main');
+    ul.addEventListener('scroll', ()=> {
+      if(ul.scrollTop ===0) {
+        this.showSearchPage = false;
+      }
+      if(ul.scrollTop > 10) {
+        this.showSearchPage = true;
+      }
+    })
+  }
+}
+</script>
+
+<style>
+ul {
+  height: calc(100vh - 94px);
+  overflow: scroll;
+
+}
+li {
+  margin-top: 20px;
+
+}
+</style>
---
+++ zm/task/slide-search/src/components/SlideSearch/index.vue
@@ -0,0 +1,88 @@
+<template>
+  <div class="search-wrap" :class="{'show-search':showSearchPage,'hide-search':!showSearchPage}">
+    <div class="title-wrap">
+      <span>标题</span>
+      <div>&gt;</div>
+    </div>
+    <div class="icon-back">
+      <span>&lt;</span>
+    </div>
+    <div class="search-input" :class="{'show-search':showSearchPage}">
+      <input type="text" class="search">
+    </div>
+ </div>
+</template>
+<script>
+export default {
+  props: {
+    showSearchPage: {
+      type: Boolean,
+      default: false
+    }
+  }
+}
+</script>
+<style>
+.search-wrap.show-search{
+  height: 52px;
+  box-shadow: 0 2px 2px 0 rgba(0, 0, 0, .1)
+}
+.search-wrap.hide-search {
+  box-shadow: none;
+}
+.search-wrap{
+  z-index: 110;
+  width: 100%;
+  height: 94px;
+  background-color: white;
+  transition: all .2 linear;
+}
+.title-wrap{
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 42px;
+  text-align: center;
+}
+.title-wrap div {
+  position: absolute;
+  height: 100%;
+  right: 0;
+  top: 0;
+  padding-right: 10px;
+}
+.icon-back{
+  position: absolute;
+  left: 10px;
+  top: 0;
+  z-index: 110;
+  height: 42px;
+}
+.search-input.show-search{
+  top: 0;
+  left: 40px;
+}
+.search-input{
+  position: absolute;
+  top: 42px;
+  left: 0;
+  z-index: 100;
+  display: flex;
+  padding: 10px;
+  width: 100%;
+  box-sizing: border-box;
+  transition: all .2 linear;
+}
+.search {
+  border: 1px solid #eee;
+  color: #666;
+  width: 100%;
+  height: 22px;
+  background: transparent;
+  font-size: 12px;
+  margin-left: 10px;
+  padding: 4px;
+  border-radius: 5px;
+}
+</style>

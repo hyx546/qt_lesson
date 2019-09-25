8663fec955419552cc5c1a1a50457a6d661cf8f0
hyx546 <love.1179612384@qq.com>
2019-09-25 20:11:13 +0800

    music

--- vue/vue-music/src/main.js
+++ vue/vue-music/src/main.js
@@ -3,16 +3,7 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import fastclick from 'fastclick'
-import VueLazyload from 'vue-lazyload'
-import def_lazy_img from '../static/img/loading.gif'
 
-Vue.use(VueLazyload,{
-  loading: def_lazy_img
-})
-
-
-fastclick.attach(document.body)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */

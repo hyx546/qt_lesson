7093c36b86af7d716b6abc274babdc84618680b2
hyx546 <love.1179612384@qq.com>
2019-10-14 15:06:40 +0800

    vue-mixin

---
+++ zm/vue-mixin/src/components/HelloWorld.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <my-dialog />
+    <my-loading />
+  </div>
+</template>
+
+<script>
+import MyDialog from './MyDialog';
+import MyLoding from './MyLoading';
+export default {
+  data () {
+    return {
+
+    }
+  },
+  components: {
+    MyLoding,
+    MyDialog
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ zm/vue-mixin/src/components/MyLoading.vue
@@ -0,0 +1,57 @@
+<template>
+  <el-table
+    v-loading="loading"
+    :data="tableData"
+    style="width: 100%">
+    <el-table-column
+      prop="date"
+      label="日期"
+      width="180">
+    </el-table-column>
+    <el-table-column
+      prop="name"
+      label="姓名"
+      width="180">
+    </el-table-column>
+    <el-table-column
+      prop="address"
+      label="地址">
+    </el-table-column>
+  </el-table>
+</template>
+
+<style>
+  body {
+    margin: 0;
+  }
+</style>
+
+<script>
+  export default {
+    data() {
+      return {
+        tableData: [{
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }, {
+          date: '2016-05-02',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }, {
+          date: '2016-05-04',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }],
+        dialogVisible:false
+      };
+    },
+     methods: {
+      open() {
+        this.dialogVisible = true
+      },
+      close () {
+        this.dialogVisible = false
+      }
+  };
+</script>

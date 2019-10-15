ad79b71a2fd5a89e5936f54f0ce46b11ce56439a
hyx546 <love.1179612384@qq.com>
2019-10-15 16:13:47 +0800

    vue-cpmponents

--- vue/vue-components/src/components/display.vue
+++ vue/vue-components/src/components/display.vue
@@ -4,7 +4,7 @@
 
 <script>
 import Vue from 'vue'
-import randomStr from '../../utils/random_str'
+import randomStr from '../utils/random_str'
 export default {
   props: {
     code: {
@@ -17,7 +17,56 @@ export default {
       html: '',
       css: '',
       js: '',
+      component: null,
+      id: randomStr()
     }
+  },
+  methods: {
+    getSource(source, type) {
+      const regex = new RegExp(`<${type}[^>]*>`)
+      let openingTag = source.match(regex)
+      if (!openingTag) return ""
+      else openingTag = openingTag[0]
+      
+      return source.slice(source.indexOf(openingTag) + openingTag.length, source.lastIndexOf(`</${type}>`))
+    },
+    splitCode() {
+      const script = this.getSource(this.code, "script").replace(
+        /export default/,
+        "return"
+      )
+      const style = this.getSource(this.code, "style")
+      const template = `<div id="app">${this.getSource(this.code,"template")}</div>`
+
+      this.html = template
+      this.css = style
+      this.js = script 
+    },
+    renderCode () {
+      this.splitCode()
+      if( this.html !== "" && this.js !==""){
+        const parseStrToFunc = new Function(this.js)()
+
+        parseStrToFunc.template = this.html
+        const Component = Vue.extend(parseStrToFunc)
+        this.component = new Component().$mount()
+        this.$refs.display.appendChild(this.component.$el)//挂载组件必须加上$el
+
+        if (this.css !== "") {
+          const style = document.createElement('style')
+          style.type = 'text/css'
+          style.id = this.id
+          style.innerHTML = this.css
+          document.getElementsByTagName('head')[0].appendChild(style)
+
+        }
+      }
+
+      
+    }
+  },
+  mounted () {
+    this.renderCode()
   }
 }
 </script>
--- vue/vue-components/src/router/index.js
+++ vue/vue-components/src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/common/switch'
+import HelloWorld from '@/view/Display'
 Vue.use(Router)
 
 export default new Router({

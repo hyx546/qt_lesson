390d0613be98a677ae5ec3409eba03a14b49cc73
hyx546 <love.1179612384@qq.com>
2019-10-15 17:18:47 +0800

    express-demo

---
+++ node/express-demo/app.js
@@ -0,0 +1,21 @@
+// let http =require('http')
+
+// let server = http.createServer()
+
+// server.on('request',(req,res) => {
+//   res.end('hello world!')
+// })
+
+// server.listen(3000, () => {
+//   console.log('服务已启动')
+// })
+
+
+const express = require('express')
+const app = express()
+app.get('/',(req,res) => {
+  res.end('hello world')
+})
+app.listen(3000, () => {
+  console.log('服务已启动')
+})
